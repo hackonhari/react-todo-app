3435feac5111af1f9023822f4c27e47162f8edf9
hackonhari <hackonhari@gmail.com>
2020-07-13 08:47:36 +0530

    Updates

---
+++ precache-manifest.95a4557029e106b15f339b04e281e672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde963005b88cba2b64776e8909d0bbe",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "a949b669d4377f22065f",
+    "url": "/react-todo-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a949b669d4377f22065f",
+    "url": "/react-todo-app/static/js/2.a7cde383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.a7cde383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3806238d679a621cda5f",
+    "url": "/react-todo-app/static/js/main.8e25ee26.chunk.js"
+  },
+  {
+    "revision": "c38f3f092b6a442ea04a",
+    "url": "/react-todo-app/static/js/runtime-main.548fee87.js"
+  }
+]);
